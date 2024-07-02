dab1a24bef07a2e1d6dbffbcaaed809cc0cc358d
sai vardhini <saivardhinirouthu5566@gmail.com>
2024-07-02 22:47:52 +0530

    1-auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
@@ -11,10 +12,12 @@ const PORT = process.env.PORT || 5000;
 
 console.log(process.env.MONGO_URI);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
-
 
 
 app.listen(PORT, () => {
