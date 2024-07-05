24da4ef23f0a998e8e6cce0940928679e79b3205
sai vardhini <saivardhinirouthu5566@gmail.com>
2024-07-05 15:33:23 +0530

    3-notifications query and mutation

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,4 +1,4 @@
-// import Notification from "../models/notification.model.js";
+import Notification from "../models/notification.model.js";
 import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 import { v2 as cloudinary } from "cloudinary";
